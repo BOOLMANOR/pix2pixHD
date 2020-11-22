f7f700df71285a91888b6961ed87129d0e66e0a7
Au <lrigi@outlook.com>
2020-11-22 16:50:41 +0800

    create_label fix

--- models/networks.py
+++ models/networks.py
@@ -82,15 +82,17 @@ class GANLoss(nn.Module):
     def get_target_tensor(self, input, target_is_real):
         target_tensor = None
         if target_is_real:
-            create_label = ((self.real_label_var is None) or
-                            (self.real_label_var.numel() != input.numel()))
+            #create_label = ((self.real_label_var is None) or
+            #                (self.real_label_var.numel() != input.numel()))
+            create_label = True
             if create_label:
                 real_tensor = self.Tensor(input.size()).fill_(self.real_label)
                 self.real_label_var = Variable(real_tensor, requires_grad=False)
             target_tensor = self.real_label_var
         else:
-            create_label = ((self.fake_label_var is None) or
-                            (self.fake_label_var.numel() != input.numel()))
+            #create_label = ((self.fake_label_var is None) or
+            #                (self.fake_label_var.numel() != input.numel()))
+            create_label = True
             if create_label:
                 fake_tensor = self.Tensor(input.size()).fill_(self.fake_label)
                 self.fake_label_var = Variable(fake_tensor, requires_grad=False)
